6b9643436f80159c4e87b03b123ba60d6dea27c9
Grant McCarty <mccar396@msu.edu>
2020-09-01 20:42:29 -0400

    adding items to group and to self now working

--- src/commands/inventory/AddItemCommand.js
+++ src/commands/inventory/AddItemCommand.js
@@ -7,21 +7,28 @@ module.exports = class AddItemCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    //message.channel.send(`Name: ${args[0]}   Value: ${value[0]}${value[1]}     Weight: ${weight[0]}${weight[1]}`);
+    //message.channel.send(`Name: ${args[0]}   Value: ${value[0]}${value[1]}     Weight: ${weight[0]}${weight[1]}`); 
     try {
       let group = args[2], id, name=args[0], quantity =args[1];
+      if(quantity === 'self' || quantity === 'group'){
+        group = quantity
+        quantity = 1;
+      }
       if(group && group === 'self'){
         id = message.member.id
       } else {
         group = 'group'
         id = message.guild.id
       }
+
+      if(!quantity) quantity = 1;
       if(group != 'group' && group != 'self') return message.channel.send("invalid group type");
 
-      item = await Items.findOne({ ownerType: group }, { ownerId: id} , { name: name });
+      let item = await Items.findOne({ ownerType: group, ownerId: id, name: name });
       if(item){
-        item.quantity = item.quantity + quantity
-        await item.save()
+        item = await Items.findOneAndUpdate({ ownerType: group, ownerId: id, name: name }, {
+          quantity: item.quantity + parseInt(quantity)
+        });
       } else {
         const item = await Items.create({
           ownerType: group,
@@ -30,8 +37,13 @@ module.exports = class AddItemCommand extends BaseCommand {
           quantity: quantity,
         })
       }
+      
       console.log("Item(s) has been added to the inventory")
+      message.channel.send("Your Items have been added to your inventory.");
+      const command = client.commands.get('view');
+      command.run(client, message, args, group);
     } catch (err) {
+      message.channel.send("An error has occured, items not added.");
       console.log(err)
     }
   }
--- src/commands/inventory/ViewInventoryCommand.js
+++ src/commands/inventory/ViewInventoryCommand.js
@@ -1,26 +1,31 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-const Items = require('../../database/schemas/Items')
+const Items = require('../../database/schemas/Items');
+const Discord = require('discord.js');
 
 module.exports = class ViewInventoryCommand extends BaseCommand {
   constructor() {
     super('view', 'inventory', []);
   }
 
-  async run(client, message, args) {
+  async run(client, message, args, group = null) {
     try {
-      let group = args[0], id;
+      if(!group) group = args[0];
+      let id;
       if(group && group === 'self'){
         id = message.member.id
       } else {
+        group = 'group'
         id = message.guild.id
       }
       if(group != 'group' && group != 'self') return message.channel.send("invalid group type");
-      itesms  = await Items.find({ownerType: group}, {ownerId: id})
+      const items  = await Items.find({ownerType: group, ownerId: id })
       if(items){
-        let messageout = '';
+        const messageOut = new Discord.MessageEmbed()
+        .setTitle('Your Inventory');
         items.forEach(item => {
-          messageout += `${item.name}: ${item.quantity} <b>`
+          messageOut.addFields({name:`${item.name}`, value:`${item.quantity}`})
         });
+        message.channel.send(messageOut);
       }
 
     } catch (err) {
